e9d73dec11082d5cbbe6229013c1a358597440fe
Kirazuto7 <62526389+Kirazuto7@users.noreply.github.com>
2022-03-30 22:09:51 -0400

    extracted debit information from api to show the list and total debit on debit page

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -3,38 +3,50 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
-/*const Debits = (props) => {
-	let debitsView = () => {
-        const { debits } = props;
-        return debits.map((debit) => {
-            let date = debit.date.slice(0,10);
-            return <li key={debit.id}>{debit.amount} {debit.description} {date}</li>
-        }) 
+class Debits extends Component {
+  constructor(props)
+  {
+    super(props);
+
+    this.state = 
+    {
+      newDebit: 
+      {
+        id: "",
+        description: "",
+        amount: -1,
+        date: ""
+      }
     }
+  }
+
+  debitsView = () => {
+    console.log(this.state.total_debit);
+    return this.props.debits.map((debit) => {
+        let date = debit.date.slice(0,10);
+        return <li key={debit.id}>{debit.amount} {debit.description} {date}</li>
+    }) 
+  }
+
+ 
+  render() {
     return (
-    	<div>
+      <div>
     	   <h1>Debits</h1>
-    	   {debitsView()}
-           <form onSubmit={props.addDebit}>
+    	 
+           <form onSubmit={this.props.addDebit}>
              <input type="text" name="description" />
              <input type="number" name="amount" />
              <button type="submit">Add Debit</button>
            </form>
+           <h3>Transaction List: {this.debitsView()}</h3>
+           
+           <h2>Total Amount of Debits: {this.props.total_debit}</h2>
+           <Link to="/">Home</Link>
     	</div>
-
-    )
-}*/
-
-class Debits extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Bank of React</h1>
-
-        <Link to="/">Home</Link>
-      </div>
     );
   }
-}
+
+} 
 
 export default Debits;
